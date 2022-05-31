6a46599e0be3613d3d018e9e3a8103507799e49c
tyrmi <tyrmit@hotmail.com>
2022-05-31 21:09:31 +1000

    Layout of home page complete for mobile view

--- src/components/Page/HomePage/HomePage.js
+++ src/components/Page/HomePage/HomePage.js
@@ -1,7 +1,23 @@
 import './HomePage.css';
+import homePageData from '../../../assets/data/home.json';
 
 function HomePage(props) {
-    return <div className="home__page">Home Page rendered</div>;
+    return (
+        <div className="home-page">
+            <section className="home-page__pretitle">
+                <h6>{homePageData.preTitle}</h6>
+            </section>
+            <section className="home-page__title">
+                <h1>{homePageData.title}</h1>
+            </section>
+            <section className="home-page__bodytext">
+                <p>{homePageData.bodyText}</p>
+            </section>
+            <section className="home-page__explore">
+                <button className="home-page__explore-btn">EXPLORE</button>
+            </section>
+        </div>
+    );
 }
 
 export default HomePage;
