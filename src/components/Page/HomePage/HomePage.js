264fb5f76052f96c3bf2ed4093fe6a3ee71ab6de
tyrmi <tyrmit@hotmail.com>
2022-06-04 00:07:31 +1000

    Layout of home page complete for tablet and desktop views

--- src/components/Page/HomePage/HomePage.js
+++ src/components/Page/HomePage/HomePage.js
@@ -4,14 +4,16 @@ import homePageData from '../../../assets/data/home.json';
 function HomePage(props) {
     return (
         <div className="home-page">
-            <section className="home-page__pretitle">
-                <h6>{homePageData.preTitle}</h6>
-            </section>
-            <section className="home-page__title">
-                <h1>{homePageData.title}</h1>
-            </section>
-            <section className="home-page__bodytext">
-                <p>{homePageData.bodyText}</p>
+            <section className="home-page__content">
+                <div className="home-page__pretitle">
+                    <h6>{homePageData.preTitle}</h6>
+                </div>
+                <div className="home-page__title">
+                    <h1>{homePageData.title}</h1>
+                </div>
+                <div className="home-page__bodytext">
+                    <p>{homePageData.bodyText}</p>
+                </div>
             </section>
             <section className="home-page__explore">
                 <button className="home-page__explore-btn">EXPLORE</button>
