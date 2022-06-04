c7b0b7875de443b63f3ffb3cf938cee0beaa2ef2
tyrmi <tyrmit@hotmail.com>
2022-06-04 11:24:31 +1000

    Added background image to crew page.

--- src/components/Page/CrewPage/CrewPage.js
+++ src/components/Page/CrewPage/CrewPage.js
@@ -1,5 +1,16 @@
+import './CrewPage.css';
+import crewPageData from '../../../assets/data/crew.json';
+
 function CrewPage(props) {
-    return <div>CrewPage</div>;
+    return (
+        <div className="crew-page">
+            <section className="crew-page__content">
+                <div className="crew-page__pretitle">
+                    <h6>{crewPageData.preTitle}</h6>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default CrewPage;
