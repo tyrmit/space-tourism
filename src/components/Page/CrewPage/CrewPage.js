f27e6e6c442060015b4016836d50efb438a8314b
tyrmi <tyrmit@hotmail.com>
2022-06-05 11:59:06 +1000

    Moved background image settings of the Crew page to the .crew-page::before rule so that the opacity can be reduced without impacting the rest of the page.

--- src/components/Page/CrewPage/CrewPage.js
+++ src/components/Page/CrewPage/CrewPage.js
@@ -3,10 +3,15 @@ import crewPageData from '../../../assets/data/crew.json';
 
 function CrewPage(props) {
     return (
-        <div className="crew-page">
+        <div className="page crew-page">
             <section className="crew-page__content">
                 <div className="crew-page__pretitle">
-                    <h6>{crewPageData.preTitle}</h6>
+                    <h6>
+                        <span className="crew-page__page-number">
+                            {crewPageData.pageNumber}
+                        </span>{' '}
+                        {crewPageData.preTitle}
+                    </h6>
                 </div>
             </section>
         </div>
